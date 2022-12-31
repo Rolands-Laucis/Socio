5a2de07635b27d378de493cf0814f7e28918055b
Rolands <rolands.laucis@gmail.com>
2022-12-31 16:09:07 +0200

    upd

--- web/src/lib/nav.svelte
+++ web/src/lib/nav.svelte
@@ -4,8 +4,8 @@
 
 <nav>
     <h1>Socio</h1>
-    <Bloom tb><a href="https://github.com/Rolands-Laucis/Socio" target="_blank">github</a></Bloom>
-    <Bloom tb><a href="https://www.npmjs.com/package/socio" target="_blank">npm</a></Bloom>
+    <Bloom style="--b_h:6px; --s_h:0.6;"><a href="https://github.com/Rolands-Laucis/Socio" target="_blank">github</a></Bloom>
+    <Bloom style="--b_h:6px; --s_h:0.6;"><a href="https://www.npmjs.com/package/socio" target="_blank">npm</a></Bloom>
 </nav>
 
 <style lang="scss">
@@ -13,9 +13,5 @@
         display: flex;
         align-items:baseline;
         gap: 24px;
-    }
-
-    a{
-        color: $acc2;
     }
 </style>
