4fd8e595cdda385b46cc155d2be3aa10317d1c96
Rolands <rolands.laucis@gmail.com>
2023-07-23 10:58:27 +0300

    comment out unneeded colors

--- core/logging.ts
+++ core/logging.ts
@@ -6,30 +6,30 @@
  */
 export const colors = {
     Reset: "\x1b[0m",
-    Bright: "\x1b[1m",
-    Dim: "\x1b[2m",
-    Underscore: "\x1b[4m",
-    Blink: "\x1b[5m",
-    Reverse: "\x1b[7m",
-    Hidden: "\x1b[8m",
+    // Bright: "\x1b[1m",
+    // Dim: "\x1b[2m",
+    // Underscore: "\x1b[4m",
+    // Blink: "\x1b[5m",
+    // Reverse: "\x1b[7m",
+    // Hidden: "\x1b[8m",
 
-    FgBlack: "\x1b[30m",
-    FgRed: "\x1b[31m",
-    FgGreen: "\x1b[32m",
-    FgYellow: "\x1b[33m",
-    FgBlue: "\x1b[34m",
-    FgMagenta: "\x1b[35m",
-    FgCyan: "\x1b[36m",
+    // FgBlack: "\x1b[30m",
+    // FgRed: "\x1b[31m",
+    // FgGreen: "\x1b[32m",
+    // FgYellow: "\x1b[33m",
+    // FgBlue: "\x1b[34m",
+    // FgMagenta: "\x1b[35m",
+    // FgCyan: "\x1b[36m",
     FgWhite: "\x1b[37m",
 
     BgBlack: "\x1b[40m",
     BgRed: "\x1b[41m",
     BgGreen: "\x1b[42m",
     BgYellow: "\x1b[43m",
-    BgBlue: "\x1b[44m",
-    BgMagenta: "\x1b[45m",
-    BgCyan: "\x1b[46m",
-    BgWhite: "\x1b[47m"
+    // BgBlue: "\x1b[44m",
+    // BgMagenta: "\x1b[45m",
+    // BgCyan: "\x1b[46m",
+    // BgWhite: "\x1b[47m"
 }
 
 //types
@@ -127,7 +127,7 @@ export class LogHandler {
 }
 
 //static f wrappers for quick log writing
-export function log(...args){LogHandler.log(...args)}
-export function info(msg:string, ...args) { LogHandler.info(msg,...args) }
-export function done(msg: string, ...args) { LogHandler.done(msg, ...args) }
-export function soft_error(msg: string, ...args) { LogHandler.soft_error(msg, ...args) }
+export function log(...args: any[]){LogHandler.log(...args)}
+export function info(msg: string, ...args: any[]) { LogHandler.info(msg,...args) }
+export function done(msg: string, ...args: any[]) { LogHandler.done(msg, ...args) }
+export function soft_error(msg: string, ...args: any[]) { LogHandler.soft_error(msg, ...args) }
